71763289a7af2fc13689028cc5dcbb5774b4ac62
Yahya Al-Abyadh <yahyaalabyadh71@gmail.com>
2023-08-30 17:57:34 +0300

    first commit

---
+++ resources/js/Pages/Home.vue
@@ -0,0 +1,5 @@
+<template>
+    <div class="text-5 bg-blue-500">
+        Hello Yahya
+    </div>
+</template>
---
+++ start/routes.ts
@@ -0,0 +1,37 @@
+/*
+|--------------------------------------------------------------------------
+| Routes
+|--------------------------------------------------------------------------
+|
+| This file is dedicated for defining HTTP routes. A single file is enough
+| for majority of projects, however you can define routes in different
+| files and just make sure to import them inside this file. For example
+|
+| Define routes in following two files
+| ├── start/routes/cart.ts
+| ├── start/routes/customer.ts
+|
+| and then import them inside `start/routes.ts` as follows
+|
+| import './routes/cart'
+| import './routes/customer''
+|
+*/
+
+import Route from '@ioc:Adonis/Core/Route'
+import User from 'App/Models/User'
+import Hash from '@ioc:Adonis/Core/Hash'
+
+Route.get('/', async ({ inertia }) => {
+  return inertia.render('Home')
+})
+
+Route.get('/users', async () => {
+  const user = await User.first();
+  if (user && await Hash.verify(user.password, "123456")) {
+    // verified
+    return user?.name
+  }
+
+  return User.all();
+})
