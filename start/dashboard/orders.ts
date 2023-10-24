f0036d01e6e4afa493aa26da705587891a1707c7
DrCinco730 <Khaled#*#738939858>
2023-10-25 02:00:14 +0300

    Order Operations

--- start/dashboard/orders.ts
+++ start/dashboard/orders.ts
@@ -1,2 +1,10 @@
 import Route from '@ioc:Adonis/Core/Route'
 Route.get('orders/handle/:id', 'dashboard/OrdersController.handleOrder')
+Route.get(
+    'orders/update/:id',
+    'dashboard/OrdersController.UpdateOrderState'
+)
+Route.get(
+    'orders/cancel/:id',
+    'dashboard/OrdersController.CancelOrder'
+)
