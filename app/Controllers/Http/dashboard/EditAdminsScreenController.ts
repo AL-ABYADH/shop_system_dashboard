d66eaf9a22b3b5272121f2da327a934b947c7b37
Mohammed Maresh <MohammedMaresh>
2023-10-24 23:59:20 +0300

    update and delete admins

--- app/Controllers/Http/dashboard/EditAdminsScreenController.ts
+++ app/Controllers/Http/dashboard/EditAdminsScreenController.ts
@@ -1,47 +1,124 @@
+// import User from 'App/Models/User';
+// import { schema, rules } from '@ioc:Adonis/Core/Validator';
+// import { HttpContextContract } from '@ioc:Adonis/Core/HttpContext';
+
+// export default class EditAdminsScreenController {
+//     public index({ inertia }) {
+//         return inertia.render('editAdminsScreen')
+//     }
+
+//     public async updateAdmin({ request, response }: HttpContextContract) {
+//         try {
+//             // Validate request data using AdonisJS validation rules
+//             const validationSchema = schema.create({
+//                 username: schema.string.optional({ trim: true }, [rules.maxLength(255)]),
+//                 // ... add other fields to validate ...
+//             });
+
+//             const data = await request.validate({
+//                 schema: validationSchema,
+//             });
+            
+//             // Find user by ID
+//             const user = await User.findOrFail(request.qs());
+
+//             // Update only non-empty fields
+//             if (data.username) user.username = data.username;
+//             // ... update other fields ...
+
+//             await user.save();
+
+//             // Fetch updated user data from the database
+//             const users = await User.all();
+            
+//             // Render Inertia page with updated data and set status code in response header
+//             response.header('X-Inertia', true);
+//             response.status(200);
+//             response.json({ component: 'editAdminsScreen', props: { users }});
+//         } catch (error) {
+//             // console.log(error);
+//             if (error.code === 'E_ROW_NOT_FOUND') {
+//                 // User with the given ID does not exist
+//                 response.header('X-Inertia', true);
+//                 response.status(404);
+//                 response.json({ component: 'editAdminsScreen', props: { error: 'User not found' } });
+//             } else if (error.code === 'E_UNIQUE_VIOLATION') {
+//                 // Unique constraint violation (user already exists)
+//                 response.header('X-Inertia', true);
+//                 response.status(400);
+//                 response.json({ component: 'editAdminsScreen', props: { error: 'User already exists' } });
+//             } else {
+//                 // Other server errors
+//                 response.header('X-Inertia', true);
+//                 response.status(500);
+//                 response.json({ component: 'editAdminsScreen', props: { error: 'Internal Server Error' } });
+//             }
+//         }
+//     }
+// }
+
 import User from 'App/Models/User';
 import { schema, rules } from '@ioc:Adonis/Core/Validator';
+import { HttpContextContract } from '@ioc:Adonis/Core/HttpContext';
 
 export default class EditAdminsScreenController {
     public index({ inertia }) {
-        return inertia.render('editAdminsScreen')
+        return inertia.render('editAdminsScreen');
     }
-    
-    public async updateAdmin({ request, inertia }) {
-        console.log('does')
-        // Validate request data using AdonisJS validation rules
-        const validationSchema = schema.create({
-            id: schema.number(),
-            username: schema.string.optional({ trim: true }, [rules.maxLength(255)]),
-            fullName: schema.string.optional({ trim: true }, [rules.maxLength(255)]),
-            phoneNumber: schema.string.optional({ trim: true }, [rules.maxLength(255)]),
-            role: schema.enum.optional(['admin', 'seller', 'customer']),
-            preferredCurrency: schema.enum.optional(['YER', 'USD', 'SAR']),
-            imageUrl: schema.string.optional({ trim: true }, [rules.maxLength(255)]),
-            shopOpenAt: schema.string.optional({ trim: true }, [rules.maxLength(255)]),
-            shopCloseAt: schema.string.optional({ trim: true }, [rules.maxLength(255)]),
-        });
 
-        const data = await request.validate({
-            schema: validationSchema,
-        });
+    public async updateAdmin({ request, response }: HttpContextContract) {
+        try {
+            const validationSchema = schema.create({
+                username: schema.string.optional({ trim: true }, [rules.maxLength(255)]),
+                // ... add other fields to validate ...
+            });
 
-        // Find user by ID
-        const user = await User.findOrFail(data.id);
+            const data = await request.validate({
+                schema: validationSchema,
+            });
 
-        // Update only non-empty fields
-        if (data.username) user.username = data.username;
-        if (data.fullName) user.fullName = data.fullName;
-        if (data.phoneNumber) user.phoneNumber = data.phoneNumber;
-        if (data.role) user.role = data.role;
-        if (data.preferredCurrency) user.preferredCurrency = data.preferredCurrency;
-        if (data.imageUrl) user.imageUrl = data.imageUrl;
-        if (data.shopOpenAt) user.shopOpenAt = data.shopOpenAt;
-        if (data.shopCloseAt) user.shopCloseAt = data.shopCloseAt;
+            const user = await User.findOrFail(request.qs().id);
 
-        await user.save();
+            if (data.username) user.username = data.username;
 
-        // Fetch updated user data from the database
-        const users = await User.all();
-        return inertia.render('editAdminsScreen', { users });
+            await user.save();
+
+            const users = await User.all();
+
+            response.header('X-Inertia', true);
+            response.status(200);
+            response.json({ component: 'editAdminsScreen', props: { users } });
+        } catch (error) {
+            if (error.code === 'E_ROW_NOT_FOUND') {
+                response.header('X-Inertia', true);
+                response.status(404);
+                response.json({ component: 'editAdminsScreen', props: { error: 'User not found' } });
+            } else if (error.code === 'E_UNIQUE_VIOLATION') {
+                response.header('X-Inertia', true);
+                response.status(400);
+                response.json({ component: 'editAdminsScreen', props: { error: 'User already exists' } });
+            } else {
+                response.header('X-Inertia', true);
+                response.status(500);
+                response.json({ component: 'editAdminsScreen', props: { error: 'Internal Server Error' } });
+            }
+        }
+    }
+
+    public async deleteAdmin({ request, response }: HttpContextContract) {
+        try {
+            const user = await User.findOrFail(request.qs().id);
+            if (user.role != 'admin') {
+                return response.status(404).json({message: 'Admin not found'})
+            }
+            await user.softDelete();
+            response.header('X-Inertia', true);
+            response.status(200);
+            response.json({ component: 'editAdminsScreen' });
+        } catch (error) {
+            response.header('X-Inertia', true);
+            response.status(400);
+            response.json({ component: 'editAdminsScreen', props: { error: 'Invalid user ID' } });
+        }
     }
 }
