4012c2407e2728de53fcbac5945f4fa2b8b1d36b
Yahya Al-Abyadh <yahyaalabyadh71@gmail.com>
2024-02-06 19:23:14 +0300

    Added auth component

---
+++ app/Controllers/Http/Auth/WebAuthController.ts
@@ -0,0 +1,28 @@
+import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
+
+export default class WebAuthController {
+    public async login({
+        request,
+        response,
+        session,
+        auth,
+    }: HttpContextContract) {
+        const { username, password } = request.only(['username', 'password'])
+        try {
+            await auth.use('web').attempt(username, password)
+        } catch (err) {
+            console.log(err)
+            session.flash({ error: 'Username or password is incorrect' })
+            response.redirect().back()
+            return response.json({ message: 'invalid username or password' })
+        }
+
+        return response.redirect().toPath('/')
+    }
+
+    public async logout({ response, auth }: HttpContextContract) {
+        await auth.logout()
+
+        return response.redirect().toPath('/login')
+    }
+}
