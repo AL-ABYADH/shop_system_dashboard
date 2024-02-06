f8e66c744a85fac7ec13fdf3eecba463e6cfbb3c
Yahya Al-Abyadh <yahyaalabyadh71@gmail.com>
2024-02-06 18:11:24 +0300

    Added UserAccounts component

---
+++ app/Controllers/Http/UserAccounts/CustomersAccounts.ts
@@ -0,0 +1,7 @@
+import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
+
+export default class CustomersAccounts {
+    public async get({ request, response }: HttpContextContract) {}
+
+    public async update({ request, response }: HttpContextContract) {}
+}
